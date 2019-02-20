df015d801038e4f6a5b22226bbcc441c9f9b569e
Tanish Manku <tanish.manku@student.ufv.ca>
2019-02-19 16:36:28 -0800

    OP6 : Bring extern references up-to-date with v1.1

--- op6/drivers/gpu/drm/msm/sde/klapse.h
+++ op6/drivers/gpu/drm/msm/sde/klapse.h
@@ -4,6 +4,6 @@
 //Required variables for external access
 extern void klapse_pulse(void);
 extern int force_livedisplay;
-extern bool brightness_lvl_auto_enable;
+extern bool brightness_factor_auto_enable;
 
 #endif	/* _LINUX_KLAPSE_H */
